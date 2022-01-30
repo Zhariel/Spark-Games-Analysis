ee16914e8235f0b4b2de2da68a200b20157bd754
Romain Blazevic <romain.blazevic@gmail.com>
2022-01-30 19:17:42 +0100

    init

---
+++ data/scripts/main.py
@@ -0,0 +1,10 @@
+import os
+
+
+def run():
+    steam = os.path.join(os.getcwd(), "..", "steam.txt")
+    twitch = os.path.join(os.getcwd(), "..", "twitch.txt")
+
+
+if __name__ == '__main__':
+    run()
